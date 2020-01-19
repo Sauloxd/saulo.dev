4f91f9a976b4fe3c1f16a80aa8db6f275244f7d0
sauloxd <saulotoshi@gmail.com>
2020-01-19 13:31:48 -0300

    Render styled-components only in page definition

--- packages/blog/gatsby-browser.js
+++ packages/blog/gatsby-browser.js
@@ -1,7 +1,6 @@
 import withGlobalStyles from '@webshine/ui/src/services/withGlobalStyles.tsx';
 import withUiConfig from '@webshine/ui/src/services/withUiConfig';
-import withNavbar from './src/hocs/withNavbar';
 
 const IdentityCompoent = ({ element }) => element;
 
-export const wrapRootElement = withUiConfig(withGlobalStyles(withNavbar(IdentityCompoent)));
+export const wrapRootElement = withUiConfig(withGlobalStyles(IdentityCompoent));
