cd0582568db54f5843972ff0d5fb22db938f0744
sauloxd <saulotoshi@gmail.com>
2020-02-16 21:29:11 -0300

    try deploying

---
+++ scritps/s3-deploy.sh
@@ -0,0 +1,14 @@
+echo "----> Deploy to s3 START"
+echo "Bucket name: $BUCKET_NAME"
+echo "Bucket region: $BUCKET_REGION"
+
+aws s3 sync build s3://$BUCKET_NAME \
+  --region=$BUCKET_REGION \
+  --delete
+
+aws s3 cp s3://$BUCKET_NAME/index.html s3://$BUCKET_NAME/index.html \
+  --metadata-directive REPLACE \
+  --cache-control max-age=0 \
+  --region=$BUCKET_REGION
+
+echo "----> Deploy to s3 END"
