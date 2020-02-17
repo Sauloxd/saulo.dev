6d1500bdece703488d49221145b005ad0828aa0a
sauloxd <saulotoshi@gmail.com>
2020-02-16 21:35:31 -0300

    update circlce

--- scritps/s3-deploy.sh
+++ scritps/s3-deploy.sh
@@ -2,9 +2,7 @@ echo "----> Deploy to s3 START"
 echo "Bucket name: $BUCKET_NAME"
 echo "Bucket region: $BUCKET_REGION"
 
-aws s3 sync build s3://$BUCKET_NAME \
-  --region=$BUCKET_REGION \
-  --delete
+aws s3 sync packages/blog/public s3://$BUCKET_NAME --region=$BUCKET_REGION --delete
 
 aws s3 cp s3://$BUCKET_NAME/index.html s3://$BUCKET_NAME/index.html \
   --metadata-directive REPLACE \
