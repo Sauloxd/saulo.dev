f81954a7682d29e3e28077ef5e0ff14406917d8e
sauloxd <saulotoshi@gmail.com>
2020-08-18 20:21:19 -0300

    bump storybook

--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -1,18 +1,7 @@
 import React, { Fragment } from 'react';
-import { configure, addDecorator } from '@storybook/react';
+import { addDecorator } from '@storybook/react';
 import withUiConfig from '../src/services/withUiConfig'
 import { Grid, CenterSingleColumn } from '../src/components/layout/Grid'
-
-const req = require.context(
-  '../src',
-  true,
-  /\.stories\.(js|jsx|ts|tsx)$/
-)
-
-configure(
-  req,
-  module
-);
 
 const UiDecorated = withUiConfig(Fragment);
 
