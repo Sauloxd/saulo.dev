4a3642db2a3c21b6b927e1fae9fe2a975084307c
sauloxd <saulotoshi@gmail.com>
2022-03-18 15:11:00 -0300

    Improve tab component to be more ReachRouter compliant

--- packages/blog/src/components/navbar/Navbar.tsx
+++ packages/blog/src/components/navbar/Navbar.tsx
@@ -12,27 +12,28 @@ const LinkStyled = styled(Link)`
   }
 `;
 const Navbar: React.FC = () => {
-  const [activeLink, setActiveLink] = useState('');
-  const generateHandleProps =
-    (tabKey: string) =>
-    (props: any): void => {
-      if (props.isPartiallyCurrent) setActiveLink(tabKey);
-    };
   const HelloTitle = 'hello.sxd';
   const BlogTitle = 'blog.sxd';
+  const generateHandleProps =
+    (tabKey: string) =>
+    (props: any): any => {
+      const active =
+        (props.isPartiallyCurrent && tabKey != HelloTitle) ||
+        (props.isCurrent && tabKey == HelloTitle);
+
+      return {
+        children: <Tab title={tabKey} active={active} />,
+      };
+    };
 
   return (
     <UiNavbar>
-      <LinkStyled to="/" getProps={generateHandleProps(HelloTitle) as any}>
-        <Tab title={HelloTitle} active={activeLink === HelloTitle} />
-      </LinkStyled>
+      <LinkStyled to="/" getProps={generateHandleProps(HelloTitle) as any} />
       <LinkStyled
         to="/blog"
         partiallyActive
         getProps={generateHandleProps(BlogTitle) as any}
-      >
-        <Tab title={BlogTitle} active={activeLink === BlogTitle} />
-      </LinkStyled>
+      />
     </UiNavbar>
   );
 };
